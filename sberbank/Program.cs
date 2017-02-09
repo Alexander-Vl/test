016ca6da6d5d6846c8278e8eed8705e9fba31eb5
Alexander_Vl <sanek.vl@mail.ru>
2017-02-09 22:35:40 +0500

    add read console more INN

--- sberbank/Program.cs
+++ sberbank/Program.cs
@@ -10,6 +10,11 @@ namespace sberbank
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+            string number  = Console.ReadLine();
+            string [] INN = number.Split(' ');
+            foreach (string INN in n)
+                list.Add(Int32.Parse(a));
         }
     }
 }
