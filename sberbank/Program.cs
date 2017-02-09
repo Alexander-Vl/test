d679e6fee1512542f4a033bf28326b76255027c9
Alexander_Vl <sanek.vl@mail.ru>
2017-02-09 23:48:15 +0500

    Revert "1"
    This reverts commit a63f2f54ecdef0aee44089fe1dc7fa9cae733890.

--- sberbank/Program.cs
+++ sberbank/Program.cs
@@ -3,40 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.S
 
 namespace sberbank
 {
-    public struct Message
-    {
-        public string Guid { get; set; }
-        public string Token { get; set; }
-    }
-
     class Program
     {
-        public void Post() {
-            Message message = new Message { };
-            string json = JsonConvert.SerializeObject(message);
-            body = Encoding.UTF8.GetBytes(json);
-            request = (HttpWebRequest)WebRequest.Create(url);
-
-            request.Method = "POST";
-            request.ContentType = "application/json";
-            request.ContentLength = body.Length;
-
-            using (Stream stream = request.GetRequestStream())
-            {
-                stream.Write(body, 0, body.Length);
-                stream.Close();
-            }
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            {
-                response.Close();
-            }
-        }
-
         static void Main(string[] args)
         {
             List<int> list = new List<int>();
